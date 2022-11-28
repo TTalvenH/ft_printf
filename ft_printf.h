04c9312d9c3b1e10fef0e2886d7462200540c020
Tuomo Talvenheimo <tuomo.talvenheimo@outlook.com>
2022-11-28 18:06:40 +0200

    init push

---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttalvenh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 12:09:47 by ttalvenh          #+#    #+#             */
+/*   Updated: 2022/11/28 12:09:48 by ttalvenh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+int ft_printf(const char *, ...);
+
+
+#endif
