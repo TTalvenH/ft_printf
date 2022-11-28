892bd52f083711586ad009bf5270aa9ca8d60ac9
Tuomo <Tuomo.talvenheimo@outlook.com>
2022-11-28 22:28:53 +0200

    not done

--- main.c
+++ main.c
@@ -5,7 +5,39 @@
 int main ()
 {
 	char	 c = 'e';
-	char	*str = "test";
-	ft_printf("tes%c", c);
+	ft_printf("ft_printf c: '%c'\n", c);
+	   printf("   printf c: '%c'\n\n", c);
+
+	char	 *s = "test";
+	ft_printf("ft_printf s: '%s'\n", s);
+	   printf("   printf s: '%s'\n\n", s);
+
+	char	 *ptr = "test";
+	ft_printf("ft_printf p: '%p'\n", ptr);
+	   printf("   printf p: '%p'\n\n", ptr);
+
+	// char	 c = 'e';
+	// ft_printf("ft_printf c: '%c'\n", c);
+	//    printf("   printf c: '%c'\n\n", c);
+
+	// char	 c = 'e';
+	// ft_printf("ft_printf c: '%c'\n", c);
+	//    printf("   printf c: '%c'\n\n", c);
+
+	// char	 c = 'e';
+	// ft_printf("ft_printf c: '%c'\n", c);
+	//    printf("   printf c: '%c'\n\n", c);
+
+	// char	 c = 'e';
+	// ft_printf("ft_printf c: '%c'\n", c);
+	//    printf("   printf c: '%c'\n\n", c);
+
+	// char	 c = 'e';
+	// ft_printf("ft_printf c: '%c'\n", c);
+	//    printf("   printf c: '%c'\n\n", c);
+
+	ft_printf("ft_printf c: '%%'\n");
+	   printf("   printf c: '%%'\n\n");
+
 	return (0);
 }
